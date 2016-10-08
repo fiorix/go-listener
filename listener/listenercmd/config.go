9e07c166dbeebacc2774726e6975247cde0e484f
Alexandre Fiori <fiorix@gmail.com>
2016-10-07 23:04:50 -0400

    Add support for prefixes

--- listener/listenercmd/config.go
+++ listener/listenercmd/config.go
@@ -17,20 +17,20 @@ type Config struct {
 	Naggle               bool   `envconfig:"TCP_NAGGLE"`
 	FastOpen             bool   `envconfig:"TCP_FAST_OPEN"`
 	TLS                  bool   `envconfig:"TLS"`
-	TLSCACertFile        string `envconfig:"TLS_CA_CERT_FILE"`
+	TLSCACertFile        string `envconfig:"TLS_CA_CERT_FILE" default:"cacert.pem"`
 	TLSClientAuth        string `envconfig:"TLS_CLIENT_AUTH"`
-	TLSCertFile          string `envconfig:"TLS_CERT_FILE"`
-	TLSKeyFile           string `envconfig:"TLS_KEY_FILE"`
+	TLSCertFile          string `envconfig:"TLS_CERT_FILE" default:"cert.pem"`
+	TLSKeyFile           string `envconfig:"TLS_KEY_FILE" default:"key.pem"`
 	LetsEncrypt          bool   `envconfig:"LETSENCRYPT"`
-	LetsEncryptCacheFile string `envconfig:"LETSENCRYPT_CACHE_FILE"`
+	LetsEncryptCacheFile string `envconfig:"LETSENCRYPT_CACHE_FILE" default:"letsencrypt.cache"`
 	LetsEncryptEmail     string `envconfig:"LETSENCRYPT_EMAIL"`
 	LetsEncryptHosts     string `envconfig:"LETSENCRYPT_HOSTS"`
 }
 
 // NewConfig creates a Config with values from environment variables.
-func NewConfig() *Config {
+func NewConfig(prefixes ...string) *Config {
 	c := &Config{}
-	envconfig.Process("", c)
+	envconfig.Process(strings.Join(prefixes, "_"), c)
 	return c
 }
 
@@ -39,7 +39,7 @@ func (c *Config) AddFlags(fs *pflag.FlagSet) {
 	fs.StringVar(&c.ListenAddr, "listen-addr", c.ListenAddr, "address in form of [ip]:port to listen on")
 	fs.BoolVar(&c.Naggle, "tcp-naggle", c.Naggle, "enable tcp nagle's algorithm")
 	fs.BoolVar(&c.FastOpen, "tcp-fast-open", c.FastOpen, "enable tcp fast open")
-	fs.BoolVar(&c.TLS, "tls", c.TLS, "enable tls")
+	fs.BoolVar(&c.TLS, "tls", c.TLS, "enable tls (requires cert file and key file)")
 	fs.StringVar(&c.TLSCACertFile, "tls-ca-cert-file", c.TLSCACertFile, "ca certificate file (for client auth)")
 	fs.StringVar(&c.TLSClientAuth, "tls-client-auth", c.TLSClientAuth, "client auth policy: RequestClientCert, RequireAnyClientCert, VerifyClientCertIfGiven, RequireAndVerifyClientCert")
 	fs.StringVar(&c.TLSCertFile, "tls-cert-file", c.TLSCertFile, "server certificate file")
